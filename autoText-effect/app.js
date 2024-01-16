4fd41edfe7ff97266fe381e2a97d5d57bab14948
Serhat Ece <134696090+serhatecee@users.noreply.github.com>
2024-01-16 16:57:24 +0300

    add-autoText-effect

---
+++ autoText-effect/app.js
@@ -0,0 +1,22 @@
+const content = document.getElementById("content");
+const speedEl = document.getElementById("speed");
+const text = "I am a computer programmer student.";
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+    content.innerText = text.slice(0, idx);
+    idx++;
+
+    if (idx > text.length) {
+        idx = 1;
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener("input", (e) => {
+    speed = 300 / e.target.value;
+})
