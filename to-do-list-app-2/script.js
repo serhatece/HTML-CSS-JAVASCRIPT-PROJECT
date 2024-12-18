3a278c6158568b38250b3e38e9d61a55984892fe
Serhat Ece <134696090+serhatece@users.noreply.github.com>
2024-12-18 15:11:31 +0300

    add-to-do-list-app-2

---
+++ to-do-list-app-2/script.js
@@ -0,0 +1,33 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length==0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML+=`
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+        `;
+
+        var current_task = document.querySelectorAll(".delete");
+        for(var i = 0; i<current_task.length; i++){
+            current_task[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length;i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value="";
+
+    }
+}
